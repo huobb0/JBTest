9100f2f58906c2b0b9fc7cdf940811172bf06790
Zsombor <Zsombor@dacookiemonsta.t.hu>
2017-09-12 10:50:40 +0200

    Initial Commit

---
+++ JBTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JBTest
+//
+//  Created by Zsombor on 2017. 09. 12..
+//  Copyright © 2017. Zsombor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JBTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JBTest
+//
+//  Created by Zsombor on 2017. 09. 12..
+//  Copyright © 2017. Zsombor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
