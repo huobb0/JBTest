bdfe7429d668b5b0084cab99bbd3ba770f82b021
huobb0 <zsombor.kovacs@gmail.com>
2019-04-21 22:04:01 +0200

    initial

--- JBTest/ViewController.m
+++ JBTest/ViewController.m
@@ -9,15 +9,31 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *JBButton;
 
 @end
 
 @implementation ViewController
 
+- (BOOL) isJailbroken{
+    NSFileManager *fileManager = [[NSFileManager alloc] init];
+    return [fileManager fileExistsAtPath:(@"/Applications/Cydia.app/Cydia")];
+}
+
+- (IBAction)ButtonPressed:(id)sender {
+    if ([self isJailbroken]) {
+        [[[UIAlertView alloc] initWithTitle:@"Result of test" message:@"Jailbroken!"
+                                    delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil] show];
+    }else{
+        [[[UIAlertView alloc] initWithTitle:@"Result of test" message:@"Clean!"
+                                    delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil] show];
+    }
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-}
+   }
 
 
 - (void)didReceiveMemoryWarning {
